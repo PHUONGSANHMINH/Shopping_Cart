c93250550bb8727907ab6210c04c2a05b54a24f1
nhikhanhh <khanhnhi931819@gmail.com>
2024-12-20 15:48:28 +0700

    33:06 vid 15

--- src/main/java/com/ecom/controller/HomeController.java
+++ src/main/java/com/ecom/controller/HomeController.java
@@ -2,16 +2,26 @@ package com.ecom.controller;
 
 import com.ecom.model.Category;
 import com.ecom.model.Product;
+import com.ecom.model.UserDtls;
 import com.ecom.repository.ProductRepository;
 import com.ecom.service.CategoryService;
 import com.ecom.service.ProductService;
+import com.ecom.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 @Controller
@@ -23,12 +33,15 @@ public class HomeController {
 	@Autowired
 	private ProductService productService;
 
+	@Autowired
+	private UserService userService;
+
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
 
-	@GetMapping("/login")
+	@GetMapping("/signin")
 	public String login() {
 		return "login";
 	}
@@ -52,5 +65,31 @@ public class HomeController {
 		Product productById = productService.getProductById(id);
 		m.addAttribute("product", productById);
 		return "view_product";
+	}
+
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute UserDtls user, @RequestParam("img") MultipartFile file, HttpSession session) throws IOException {
+		String imageName = file.isEmpty() ? "default.jpg" : file.getOriginalFilename();
+		user.setProfileImage(imageName);
+		UserDtls saveUser = userService.saveUser(user);
+
+		if (!ObjectUtils.isEmpty(saveUser))
+		{
+			if (!file.isEmpty())
+			{
+				File saveFile = new ClassPathResource("static/img/").getFile();
+
+				Path path = Paths.get(saveFile.getAbsolutePath() + File.separator + "profile_img" + File.separator
+						+ file.getOriginalFilename());
+
+				System.out.println(path);
+				Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
+			}
+			session.setAttribute("succMsg", "Register Successfully");
+		} else {
+			session.setAttribute("errMsg", "Something went wrong");
+		}
+		return "redirect:/register";
 	}
 }
---
+++ src/main/java/com/ecom/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ecom.repository;
+
+import com.ecom.model.UserDtls;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserDtls, Integer> {
+
+    public UserDtls findByEmail(String email);
+
+
+}
